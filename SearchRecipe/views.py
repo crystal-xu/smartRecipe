207510dc707457f9fef893312bef9e49b7a10c4f
刘琦 <liuqi@liuqideMacBook-Pro.local>
2020-01-27 23:00:55 +0800

    demo

--- SearchRecipe/views.py
+++ SearchRecipe/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from bson import json_util
+from django.core.paginator import Paginator
+from django.shortcuts import HttpResponse, render
 
 # Create your views here.
+from SearchRecipe.models import TestModel
+
+
+def add(request):
+    TestModel.objects.create(
+        title="news2",
+        text="xxxxxxx",
+        link="http://www.baidu.com"
+    )
+    return HttpResponse("hello mongodb")
+
+
+def query(request):
+    result = TestModel.objects.all()
+    return HttpResponse(json_util.dumps(result._collection_obj.find(result._query)),
+                        content_type="application/json")
+
+
+def search(request):
+    q = request.GET.get('q')
+    if q:
+        question_list = TestModel.objects.filter(title=q)
+    else:
+        question_list = TestModel.objects.all()
+    paginator = Paginator(question_list, 2)
+    context = {
+        'page': paginator.page(request.GET.get('page', 1)),
+        'paginator': paginator,
+        'query': query
+    }
+    return render(request, 'index.html', context)
