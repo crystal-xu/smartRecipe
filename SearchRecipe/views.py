b415662cf8f27410c36d0afa61994f3df3ee9d6e
mac <mac@lijhdeMacBook-Pro.local>
2020-03-09 00:39:26 +0000

    fix search problem

--- SearchRecipe/views.py
+++ SearchRecipe/views.py
@@ -59,7 +59,10 @@ def search(request):
         paginator = Paginator(question_list, 5)
 
     print("page: {}".format(page))
-    q_url = quote(q, 'utf-8')
+    if q is not None:
+        q_url = quote(q, 'utf-8')
+    else:
+        q_url = q
     context = {
         'page': paginator.page(request.GET.get('page', page)),
         'paginator': paginator,
